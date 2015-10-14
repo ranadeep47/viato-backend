1018a8ac34ee3bf2225fb26bf1192e0eaca21f6d
Ranadeep <ranadeep.mobile@gmail.com>
2015-10-14 18:30:05 +0530

    meh

--- admin/content/index.js
+++ admin/content/index.js
@@ -2,9 +2,61 @@ var db = require('../../db');
 var Router = require('koa-router');
 var content = new Router();
 
+var storeImage = require('./storeImage');
+
 module.exports = content;
 
 
 content.get('/', function*() {
-  yield this.render('home');
+  var docs = yield db.Feed.find().select('-list').exec();
+  yield this.render('content-home', {categories : docs});
+})
+
+content.get('/category/add', function*(){
+  yield this.render('add-category');
+})
+
+//Add a category
+content.post('/category/add', function*(){
+  var title = this.request.body['title'];
+  var image = this.request.body['image'];
+  if(!title || !image) return this.throw(400);
+
+  //Fetch, resize and store cover images
+  var category = yield storeImage(image)
+  .then(function(image){
+    return db.Feed.create({
+      title : title,
+      image : image,
+      list : []
+    });
+  });
+
+  this.redirect('/admin/content/category/'+category['_id']);
+})
+
+
+//Get a category detail page
+content.get('/category/:catId', function*(){
+  this.body = this.params['catId'];
+})
+
+//Update contents details
+content.post('/category/:catId', function*() {
+
+})
+
+//Add items to categories from amazonIds
+content.post('/category/items', function*() {
+
+})
+
+//Get an items details from the category
+content.get('/category/:catId/item/:itemId', function*(){
+
+})
+
+//Update an item's detail - Modify catalogue and refresh the item in all categories
+content.post('/category/:catId/item/:itemId', function*(){
+
 })
---
+++ admin/content/storeImage.js
@@ -0,0 +1,32 @@
+var request = require('request');
+
+var path = require('path');
+var uuid = require('node-uuid');
+var fs = require('fs');
+var request = require('request');
+var sharp = require('sharp');
+
+var IMAGES_DIR = '/home/viato/images/categories/';
+
+module.exports = storeImage;
+
+function storeImage(link) {
+  var cover = uuid.v4() + '.jpg';
+  var square = uuid.v4() + '.jpg';
+
+  return new Promise(function(resolve, reject) {
+    var image = request(link).on('error', function(){resolve(null)})
+
+    image
+    .pipe(sharp().resize(720,360).on('error', function(err){ console.log(err) }))
+    .pipe(fs.createWriteStream(IMAGES_DIR + cover));
+
+    image
+    .pipe(sharp().resize(360,360).on('error', function(err){ console.log(err) }))
+    .pipe(fs.createWriteStream(IMAGES_DIR + square));
+
+    setTimeout(function(){
+      resolve({cover : cover, square : thumb});
+    }, 0)
+  });
+}
