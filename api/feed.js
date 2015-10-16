eb41b14cb37f31344b740f216f71e359505a5c1e
Ranadeep <ranadeep.mobile@gmail.com>
2015-10-16 17:34:47 +0530

    meh

--- api/feed.js
+++ api/feed.js
@@ -10,13 +10,8 @@ feed.get('/home', function*() {
   this.body = yield db.Feed.find().select('-list -__v').exec();
 })
 
-feed.get('category/trending', function*() {
-  //TODO : Have trending category ?
-  var Id = '561e5ca78669ae5535cff4f3';
-  this.body = yield db.Feed.findOne({_id : Id }).exec();
-});
-
 feed.get('/category/:id', function*() {
   var categoryId = this.params['id'];
+  if(categoryId === 'trending') categoryId = '561e5ca78669ae5535cff4f3';
   this.body = yield db.Feed.findOne({_id : categoryId}).exec();
 })
