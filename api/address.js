fcf863c8e2db208b492058be9fc280c850d2cac0
Ranadeep <ranadeep.mobile@gmail.com>
2015-10-18 19:01:37 +0530

    updated address body schema

--- api/address.js
+++ api/address.js
@@ -16,15 +16,13 @@ address.get('/', function*(){
 
 address.post('/', function*(){
   var userId = this.state.user['userId'];
-  if(!utils.checkBody(['flat','street','placeId'], this.request.body)) return this.throw(400);
+  if(!utils.checkBody(['flat','street','locality'], this.request.body)) return this.throw(400);
 
   var address = {};
   address['flat']     = this.request.body['flat'];
   address['street']   = this.request.body['street'];
   address['label']    = this.request.body['label'] || '';
-  address['locality'] = {};
-  address['locality']['placeId']  = this.request.body['placeId'];
-  address['locality']['name']     = this.request.body['locality'] || '';
+  address['locality'] = this.request.body['locality'];
   //TODO Validate above fields
   address = yield db.User.addAddress(userId, address)
   .then(function(user){
@@ -32,23 +30,19 @@ address.post('/', function*(){
   });
 
   this.body = address;
-})
+});
 
 address.put('/:addressId', function*(){
   var userId     = this.state.user['userId'];
   var addressId  = this.params['addressId'];
-  if(!utils.checkBody(['flat','street','label'], this.request.body)) return this.throw(400);
+  if(!utils.checkBody(['flat','street','label','locality'], this.request.body)) return this.throw(400);
 
   var address = {};
   address['flat']     = this.request.body['flat'];
   address['street']   = this.request.body['street'];
-  address['label']    = this.request.body['label'];
-
-  if(this.request.body['placeId']){
-    address['locality'] = {};
-    address['locality']['placeId']  = this.request.body['placeId'];
-    address['locality']['name']     = this.request.body['locality'] || '';
-  }
+  address['label']    = this.request.body['label'] || '';
+  address['locality'] = this.request.body['locality'];
+  
   //TODO Validate above fields
   this.body = yield db.User.updateAddress(userId, addressId, address)
   .then(function(updatedAdress){
