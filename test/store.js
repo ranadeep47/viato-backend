b88ab43b0c4acbdc8b4c1d1d5f7e120c0ef4ea22
Ranadeep <ranadeep.mobile@gmail.com>
2015-10-18 20:24:37 +0530

    added gm

--- test/store.js
+++ test/store.js
@@ -2,7 +2,7 @@ var path = require('path');
 var uuid = require('node-uuid');
 var fs = require('fs');
 var request = require('request');
-var sharp = require('sharp');
+gm = require('gm');
 
 var COVER_DIR = '/home/viato/images/test/';
 var THUMBS_DIR = '/home/viato/images/test/';
@@ -18,14 +18,34 @@ function storeImage(link) {
   var image = request(link).on('error', function(){resolve(null)})
 
   //image.pipe(fs.createWriteStream(COVER_DIR + cover));
+  gm(image)
+  .resize('98', '150', '^')
+  .gravity('Center')
+  .crop('98', '150')
+  .write(fs.createWriteStream(THUMBS_DIR + thumb1));
 
-  image.pipe(sharp().resize(98,150).on('error', function(err){ console.log(err) }))
-  .pipe(fs.createWriteStream(THUMBS_DIR + thumb1));
+  gm(image)
+  .resize('147', '225', '^')
+  .gravity('Center')
+  .crop('147', '225')
+  .write(fs.createWriteStream(THUMBS_DIR + thumb2));
 
-  image.pipe(sharp().resize(147,225).on('error', function(err){ console.log(err) }))
-  .pipe(fs.createWriteStream(THUMBS_DIR + thumb2));
+  gm(image)
+  .resize('196', '300', '^')
+  .gravity('Center')
+  .crop('196', '300')
+  .write(fs.createWriteStream(THUMBS_DIR + thumb3));
 
-  image.pipe(sharp().resize(196,300).on('error', function(err){ console.log(err) }))
-  .pipe(fs.createWriteStream(THUMBS_DIR + thumb3));
+
+
+
+  // image.pipe(sharp().resize(98,150).on('error', function(err){ console.log(err) }))
+  // .pipe(fs.createWriteStream(THUMBS_DIR + thumb1));
+  //
+  // image.pipe(sharp().resize(147,225).on('error', function(err){ console.log(err) }))
+  // .pipe(fs.createWriteStream(THUMBS_DIR + thumb2));
+  //
+  // image.pipe(sharp().resize(196,300).on('error', function(err){ console.log(err) }))
+  // .pipe(fs.createWriteStream(THUMBS_DIR + thumb3));
 
 }
