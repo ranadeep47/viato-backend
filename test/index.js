f8591e9459e5a69ffbf3bb5bae078a0165413d5c
Ranadeep <ranadeep.mobile@gmail.com>
2015-10-19 17:46:44 +0530

    a

--- test/index.js
+++ test/index.js
@@ -1,9 +1,28 @@
 var fetch = require('./goodreads');
 
-fetch('http://www.goodreads.com/book/show/25916133-aarushi')
-.then(function(l){
-  console.log(l);
+
+
+var isbn = [
+  '9780765328342', '9780330391566',
+  '9780099416159', '9781567922813',
+  '9780330525602', '9780755354061',
+  '9780425212783', '9788192877440',
+  '9780099282648', '9780330419024'
+];
+
+
+
+var links = isbn.map(function(i){ return "http://www.goodreads.com/search?utf8=%E2%9C%93&query="+i});
+
+var pros = links.map(function(l){ return fetch(l)});
+
+console.time('start')
+Promise.all(pros).then(function(r){
+  console.timeEnd('start');
+  console.log(r);
 })
-.catch(function(e){
+.catch(log)
+
+function log(e){
   console.log(e);
-})
+}
