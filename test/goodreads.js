5ce0ece99e5be14ecf5806c4525c869e94d2fcf8
Ranadeep <ranadeep.mobile@gmail.com>
2015-10-19 18:23:57 +0530

    a

--- test/goodreads.js
+++ test/goodreads.js
@@ -8,10 +8,20 @@ function fetch(url) {
     request.get(url,function(e,r,d){
       if(e) return reject(e);
        var $ = cheerio.load(d);
+       var rating       = parseFloat($('.average').text())
+       var ratingsCount = parseInt($('.votes').text());
+       var reviewCount  = parseInt($('.count').text());
+       var description  = $('#description span~span').text() || '';
+
+       if(isNaN(rating)) rating = 0;
+       if(isNaN(ratingsCount)) ratingsCount = 0;
+       if(isNaN(reviewCount)) reviewCount = 0;
+
        var vals = {
-         rating : parseFloat($('.average').text()),
-         ratingsCount : parseInt($('.votes').text()),
-         reviewCount : parseInt($('.count').text())
+         rating : rating,
+         ratingsCount : ratingsCount,
+         reviewCount : reviewCount,
+         description : description
        }
       resolve(vals);
     })
