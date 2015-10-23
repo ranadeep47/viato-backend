09fc5d8c509713c5419ee070643fb816f2903f28
Ranadeep <ranadeep.mobile@gmail.com>
2015-10-23 10:11:54 +0530

    a

--- admin/booking/index.js
+++ admin/booking/index.js
@@ -2,6 +2,15 @@ var db = require('../../db');
 var Router = require('koa-router');
 var booking = new Router();
 
+booking.get('/', function*(){
+  var CompleteStatus = ['COMPLETED', 'CANCELLED'];
+  db.Booking
+    .find({status : {$nin : CompleteStatus}})
+    .populate('user_id', 'name')
+    .sort({booked_at : -1})
+    .exec().then(function(bookings){
 
+  });
+})
 
 module.exports = booking;
