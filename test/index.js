3b47c7e7c41ee4818264af63d20a6011143e4eaa
Ranadeep <ranadeep.mobile@gmail.com>
2015-10-23 15:58:02 +0530

    test

--- test/index.js
+++ test/index.js
@@ -10,24 +10,40 @@ var offset = storage.getItemSync('offset');
 get()
 
 function get(){
-  return db.Catalogue.find({description : ''}).select('isbn13').sort({_id : 1}).skip(offset).limit(10).exec().then(function(docs){
-    var links = docs.map(function(i){ return "http://www.goodreads.com/search?utf8=%E2%9C%93&query="+i.isbn13});
-    var promises = links.map(function(l){ return fetch(l)});
-    return Promise.all(promises).then(function(results){
-      var pros = [];
-      var result, doc;
-      for(var i =0; i < results.length; ++i ) {
-        result = results[i];
-        doc = docs[i];
-        pros.push(update(result, doc._id));
-      }
-      return Promise.all(pros).then(function(){
-        console.log('Completed : ', offset);
-        offset += 10;
-        storage.setItemSync('offset', offset);
-        get();
+  return db.Catalogue
+    .find({description : '', 'popularity.reviewCount' : 0})
+    .select('title')
+    .sort({_id : 1})
+    .skip(offset)
+    .limit(10).exec().then(function(docs){
+      var links = docs.map(function(i){ return "https://www.goodreads.com/search?utf8=%E2%9C%93&search_type=books&search%5Bfield%5D=title&q="+i.title});
+      var promises = links.map(function(l){
+        return axios.get(l).then(function(res){
+          var $ = cheerio.load(res.data);
+          return "https://www.goodreads.com" + $('.bookTitle').eq(0).attr('href');
+        })
+      });
+
+      return Promise.all(promises).then(function(results){
+        console.log('Links \n' results);
+        promises  = results.map(function(l) { return fetch(l); });
+        return Promise.all(promises).then(function(results){
+          var pros = [];
+          var result, doc;
+          for(var i =0; i < results.length; ++i ) {
+            result = results[i];
+            doc = docs[i];
+            return console.log(result);
+            pros.push(update(result, doc._id));
+          }
+          return Promise.all(pros).then(function(){
+            console.log('Completed : ', offset);
+            offset += 10;
+            storage.setItemSync('offset', offset);
+            get();
+          })
+        })
       })
-    })
   })
 }
 
