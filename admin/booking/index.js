2d8093a24bf72a9c29f8021f08881e79ebdf458e
Ranadeep <ranadeep.mobile@gmail.com>
2015-10-28 19:53:18 +0530

    a

--- admin/booking/index.js
+++ admin/booking/index.js
@@ -9,8 +9,14 @@ var _ = require('lodash');
 */
 
 booking.get('/', function*(){
-  var placed = getBookings('PLACED');
-  placed.then(function(docs){
+  var notCompletedStatus = ['PLACED', 'CONFIRMED', 'DISPATCHED', '']
+  // var placed = getBookings('PLACED');
+  // placed.then(function(docs){
+  //   console.log(docs);
+  // })
+
+  var pickups = getPickups();
+  pickups.then(function(docs){
     console.log(docs);
   })
 });
@@ -42,6 +48,33 @@ function getBookings(status, offset, limit) {
   .catch(function(err){
     console.log(err);
     return [];
+  })
+}
+
+function getPickups(offset, limit){
+  var offset = offset || 0;
+  var limit = limit || 0;
+
+  return db.Booking.find({'rentals.status' : 'SCHEDULED FOR PICKUP'})
+  .select('user_id order_id delivery_address rentals')
+  .populate('user_id', 'mobile email')
+  .skip(offset)
+  .limit(limit)
+  .exec()
+  .then(function(docs){
+    var docs = docs.map(function(doc){
+      var rentals = _.pluck(doc.rentals, 'item expired_at');
+      var address = doc['delivery_address'];
+      address = [address['flat'], address['street'], address['locality']['name']].join(',');
+      return {
+        userEmail   : doc['user_id']['email']['email'],
+        userMobile  : doc['user_id']['mobile'],
+        userAddress : address,
+        rentals     : rentals
+      }
+    });
+
+    return docs;
   })
 }
 
