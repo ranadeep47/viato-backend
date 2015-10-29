40c67d1bd05cc4d2917ee690e5e138cbd6a8b78b
Ranadeep <ranadeep.mobile@gmail.com>
2015-10-29 17:20:34 +0530

    a

--- services/expireCheckCron.js
+++ services/expireCheckCron.js
@@ -4,7 +4,7 @@ var moment = require('moment');
 
 var job = new CronJob(
   // '1 0 * * *', // Run it every 00:01:00 minute of everyday
-  '20 17 * * *', // Run it every 00:01:00 minute of everyday
+  '22 17 * * *', // Run it every 00:01:00 minute of everyday
   checkExpires,
   true, /* Start the job right now */
   'Asia/Kolkata' /* Time zone of this job. */
@@ -17,12 +17,12 @@ function checkExpires(){
   today = today.toDate();
   tomorrow = tomorrow.toDate();
 
-  db.Booking.find(
-    {'rentals.status'     : {$in : ['READING', 'READING-EXTENDED']},
-    {'rentals.expires_at' : {$gte : today , $lt : tomorrow}}
+  db.Booking.find({
+    'rentals.status'     : {$in : ['READING', 'READING-EXTENDED']},
+    'rentals.expires_at' : {$gte : today , $lt : tomorrow}
   })
-  .select('rentals').
-  exec()
+  .select('rentals')
+  .exec()
   .then(function(Bookings){
     Bookings.forEach(function(booking){
       var rentals = booking.rentals;
