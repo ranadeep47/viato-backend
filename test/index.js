18c5bc9920fa1a3e9436a86bd35e71b07fe1d8a4
Ranadeep <ranadeep.mobile@gmail.com>
2015-10-29 22:23:56 +0530

    a

--- test/index.js
+++ test/index.js
@@ -1,3 +1,6 @@
+var env = process.env['NODE_ENV'];
+var config = require('./config')[env];
+global.config = config;
 var amazonFetch = require('../services/amazon');
 
 amazonFetch("0062273205").then(function(book){
