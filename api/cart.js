2df96c1db5e844cbebb231f4b3787dc54ef9cf1f
Ranadeep <ranadeep.mobile@gmail.com>
2015-11-04 19:56:39 +0530

    a

--- api/cart.js
+++ api/cart.js
@@ -35,7 +35,7 @@ cart.post('/', function*(){
     return db.User.addToCart(userId, basicItem).then(function(cart){
       return cart;
     }).catch(function(e){
-      return ctx.throw(400, e);
+      return ctx.throw(400, e.message);
     })
   });
 
