f9981bb76bea74a80289f957feaa8a848a475322
Ranadeep <ranadeep.mobile@gmail.com>
2015-11-06 15:13:47 +0530

    a

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ var api = new Router();
 
 module.exports = api;
 
+api.use('/version', require('./versioning'));
 api.use('/geo', require('./geo').routes());
 api.use('/books', require('./books').routes());
 api.use('/feed', require('./feed').routes());
