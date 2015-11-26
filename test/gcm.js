654a58093b70c9ad496e4884ba57979b1a784a00
Ranadeep <ranadeep.mobile@gmail.com>
2015-11-26 18:37:23 +0530

    gcm test

--- test/gcm.js
+++ test/gcm.js
@@ -1,4 +1,5 @@
 var gcm = require('node-gcm');
+var _ = require('lodash');
 
 var message = new gcm.Message({
     // collapseKey: 'demo',
@@ -9,15 +10,20 @@ var message = new gcm.Message({
     restrictedPackageName: "in.viato.app",
     //dryRun: true,
     data: {
-      title: "Rent a bestseller for the weekend!",
-      message: "Get ready for an exciting weekend, rent and read a bestseller today !"
+      title: "Read a book this weekend!",
+      message: "Get ready for an exciting weekend, rent and read your favorite book today !"
     }
 });
 
-var t = ['']
+var db = require('../db');
 var sender = new gcm.Sender('AIzaSyBo8mYCRkA9EBp8kjrqbu43C-GadTtVO64');
 
-sender.send(message, { registrationIds: t }, function (err, result) {
-    if(err) console.error(err);
-    else  console.log(result);
-});
+
+db.User.find().select('devices').exec().then(function(Users){
+  var t = _.pluck(Users, 'app_token');
+  return console.log(t);
+  sender.send(message, { registrationIds: t }, function (err, result) {
+      if(err) console.error(err);
+      else  console.log(result);
+  });
+})
