8c5fbaecaad286b480871b68ea3fb0d0ae507b4a
Ranadeep <ranadeep.mobile@gmail.com>
2015-11-26 18:31:38 +0530

    gcm test

--- test/gcm.js
+++ test/gcm.js
@@ -9,19 +9,15 @@ var message = new gcm.Message({
     restrictedPackageName: "in.viato.app",
     //dryRun: true,
     data: {
-      title: "Hello, World",
-      icon: "ic_launcher",
-      message: "This is a notification that will be displayed ASAP.",
-      click_action: "in.viato.app.CATEGORY",
-      categoryId: "56368178a4824eb615732bd4",
-      categoryName: "Politics & History"
+      title: "Rent a bestseller for the weekend!",
+      message: "Get ready for an exciting weekend, rent and read a bestseller today !"
     }
 });
 
-var regTokens = ['f8GkzMLg7j8:APA91bHlD3QaPIW8Bhv6pKxG1OOhoNtnHOEaEWmc9UPqCRDgFkBZnyX09mhiukZSXXsntXlp4MQI_7x8X9fv2TdZ_L217CZEI6pzVnX0ZyTZozN0DmVd65X67xZAVuKpvTeJ6Z4VGGzc'];
+var t = ['']
 var sender = new gcm.Sender('AIzaSyBo8mYCRkA9EBp8kjrqbu43C-GadTtVO64');
 
-sender.send(message, { registrationIds: regTokens }, function (err, result) {
+sender.send(message, { registrationIds: t }, function (err, result) {
     if(err) console.error(err);
     else  console.log(result);
 });
